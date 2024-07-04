21fc9ae2136cfc14c9bc40391756171b3c401ec4
imarin309 <16imarin309@gmail.com>
2024-07-04 21:33:58 +0900

    make view

--- app/controllers/interview_controller.rb
+++ app/controllers/interview_controller.rb
@@ -6,4 +6,26 @@ class InterviewController < ApplicationController
   def interview_introduction
     @question = view_context.extract_question_introduction
   end
+
+  def interview_work
+    @question = view_context.extract_question_work
+  end
+
+  def interview_personal
+    @question = view_context.extract_question_personal
+  end
+
+  def interview_communication
+    @question = view_context.extract_question_communication
+  end
+
+  def interview_engineer
+    @question = view_context.extract_question_engineer
+  end
+
+  def interview_career
+    @question = view_context.extract_question_career
+  end
+
+  def end; end
 end
