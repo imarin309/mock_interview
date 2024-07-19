9301bb11a8f571c838a35d699c1900abc6c1fd90
imarin309 <16imarin309@gmail.com>
2024-07-19 21:43:06 +0900

    add text box

--- app/controllers/interview_controller.rb
+++ app/controllers/interview_controller.rb
@@ -3,6 +3,30 @@
 class InterviewController < ApplicationController
   def index; end
 
+  def submit
+    @textbox_value = params[:textbox]
+    @source_view = params[:source_view]
+    Rails.logger.debug(@source_view)
+
+    case @source_view # TODO: 綺麗に書く、ロジックを移動させる
+    when 'introduction'
+      link_to_url = '/interview/interview_work'
+    when 'work'
+      link_to_url = '/interview/interview_personal'
+    when 'personal'
+      link_to_url = '/interview/interview_communication'
+    when 'communication'
+      link_to_url = '/interview/interview_engineer'
+    when 'engineer'
+      link_to_url = '/interview/interview_career'
+    when 'career'
+      link_to_url = '/interview/end'
+    else
+      Rails.logger.debug('TODO: 例外処理を追加')
+    end
+    redirect_to link_to_url
+  end
+
   def interview_introduction
     @question = view_context.extract_question_introduction
   end
